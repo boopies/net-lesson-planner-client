a815fae3bc70cc64f9e81aaf59c7b3e226cf9c5b
boopies <tmn.sensei@gmail.com>
2020-03-04 03:10:17 +0900

    fixed savedlesson smoke test

--- src/Savedlessons/SavedLessons.test.js
+++ src/Savedlessons/SavedLessons.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SavedLessons from './SavedLessons'
+import Savedlessons from './SavedLessons'
 import {BrowserRouter} from "react-router-dom";
 
 
 describe('Saved Lessons Component', () => {
     it('renders without crashing', () => {
       const div = document.createElement('div');
-      ReactDOM.render(<BrowserRouter><SavedLessons /></BrowserRouter>, div);
+      ReactDOM.render(<BrowserRouter><Savedlessons /></BrowserRouter>, div);
       ReactDOM.unmountComponentAtNode(div);
     });
   });
